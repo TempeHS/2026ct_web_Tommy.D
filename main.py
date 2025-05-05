93b6589ac1d811d125bef0de2711ab71f0f4febc
tommytech1 <tommy.dockrill@education.nsw.gov.au>
2025-05-05 02:48:10 +0000

    Create template and menu

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import render_template
 
-app = Flask (__name__)
+app = Flask(__name__)
 
+@app.route('/index.html')
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html"), 200
 
 if __name__ == '__main__':
